2c0b5ca4ecc0ca41bbdda9406b861e284cdfb0f9
sushil kumar <sushil@gmail.com>
2021-08-24 09:52:42 +0530

    rough commited

---
+++ rough.js
@@ -0,0 +1,23 @@
+// function titleCase(str) {
+//   var convertToArray = str.toLowerCase().split(" ");
+//   // console.log(convertToArray)
+// //   var result = convertToArray.map(function(val) {
+// for(val of convertToArray)      
+// console.log(val)
+// }
+
+//     return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+//   });
+//   return result.join(" ");
+// }
+
+// titleCase("I'm a little tea pot");
+    
+    let str="I'm a little tea pot"
+    let convertToArray = str.toLowerCase().split(" ");
+    for (var i = 0; i < convertToArray.length; i++) {
+     console.log(convertToArray[i])
+    }
+
+    
+    
